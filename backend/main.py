da15a535ea4a9d386fee172dba7951b70a4a29cb
KarlaKeurin <karlakeurinoliveira@gmail.com>
2025-04-05 18:32:43 -0300

    Melhorando o código

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
+from unidecode import unidecode
 
 app = FastAPI()
 
-# Configuração do CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -15,7 +15,11 @@ app.add_middleware(
 
 df = pd.read_csv("../arquivos/operadoras_de_plano_de_saude_ativas/Relatorio_cadop.csv", sep=";")
 
+df['Nome_Fantasia'] = df['Nome_Fantasia'].astype(str).str.strip()
+df['Nome_Fantasia_Sem_Acento'] = df['Nome_Fantasia'].apply(lambda x: unidecode(x.lower()) if isinstance(x, str) else x)
+
 @app.get("/buscar")
 def buscar_operadora(nome: str = Query(..., description="Nome da operadora")):
-    resultado = df[df['Nome_Fantasia'].str.contains(nome, case=False, na=False)]
-    return resultado.fillna("").to_dict(orient='records')
+    nome_sem_acento = unidecode(nome.strip().lower())
+    resultado = df[df['Nome_Fantasia_Sem_Acento'].str.contains(nome_sem_acento, na=False)]
+    return resultado.drop(columns=['Nome_Fantasia_Sem_Acento']).fillna("").to_dict(orient='records')
